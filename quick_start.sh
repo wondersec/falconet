6c7a95990d0647e667d70acf0b84470b38f61fe0
wondersec <chenhua@wondersec.com>
2021-12-22 14:34:46 +0800

    add git attributes

--- quick_start.sh
+++ quick_start.sh
@@ -14,43 +14,43 @@ function color_log()
 function prepare_check() {
   isRoot=`id -u -n | grep root | wc -l`
   if [ "x$isRoot" != "x1" ]; then
-      echo -e $"[\033[31m ERROR \033[0m] Please use root to execute the installation script (请用 root 用户执行安装脚本)"
-      exit 1
+    echo -e $"[\033[31m ERROR \033[0m] Please use root to execute the installation script (请用 root 用户执行安装脚本)"
+    exit 1
   fi
   processor=`cat /proc/cpuinfo| grep "processor"| wc -l`
   #processor=4
   if [ $processor -lt 2 ]; then
-      echo -e "[\033[31m ERROR \033[0m] The CPU is less than 2 cores (CPU 小于 2核，Falconet 所在机器的 CPU 需要至少 2核)"
-      exit 1
+    echo -e "[\033[31m ERROR \033[0m] The CPU is less than 2 cores (CPU 小于 2核，Falconet 所在机器的 CPU 需要至少 2核)"
+    exit 1
   fi
   memTotal=`cat /proc/meminfo | grep MemTotal | awk '{print $2}'`
   #memTotal=15000000
   if [ $memTotal -lt 7500000 ]; then
-      echo -e "[\033[31m ERROR \033[0m] Memory less than 8G (内存小于 8G，Falconet 所在机器的内存需要至少 8G)"
-      exit 1
+    echo -e "[\033[31m ERROR \033[0m] Memory less than 8G (内存小于 8G，Falconet 所在机器的内存需要至少 8G)"
+    exit 1
   fi
 }
 
 function install_soft() {
-    if command -v dnf > /dev/null; then
-      if [ "$1" == "python" ]; then
-        dnf -q -y install python2
-        ln -s /usr/bin/python2 /usr/bin/python
-      else
-        dnf -q -y install $1
-      fi
-    elif command -v yum > /dev/null; then
-      yum -q -y install $1
-    elif command -v apt > /dev/null; then
-      apt-get -qqy install $1
-    elif command -v zypper > /dev/null; then
-      zypper -q -n install $1
-    elif command -v apk > /dev/null; then
-      apk add -q $1
-    else
-      echo -e "[\033[31m ERROR \033[0m] Please install it first (请先安装) $1 "
-      exit 1
-    fi
+  if command -v dnf > /dev/null; then
+  if [ "$1" == "python" ]; then
+    dnf -q -y install python2
+    ln -s /usr/bin/python2 /usr/bin/python
+  else
+    dnf -q -y install $1
+  fi
+  elif command -v yum > /dev/null; then
+  yum -q -y install $1
+  elif command -v apt > /dev/null; then
+  apt-get -qqy install $1
+  elif command -v zypper > /dev/null; then
+  zypper -q -n install $1
+  elif command -v apk > /dev/null; then
+  apk add -q $1
+  else
+  echo -e "[\033[31m ERROR \033[0m] Please install it first (请先安装) $1 "
+  exit 1
+  fi
 }
 
 function prepare_install() {
@@ -63,7 +63,7 @@ function config_docker() {
   if [ ! -f "/etc/docker/daemon.json" ]; then
     mkdir -p /etc/docker/
     wget -qO /etc/docker/daemon.json https://wondersec.falconet.org/download/docker/daemon.json || {
-      rm -f /etc/docker/daemon.json
+    rm -f /etc/docker/daemon.json
     }
   fi
 }
@@ -73,14 +73,14 @@ function get_package() {
   cd /opt
   if [ ! -d "/opt/falconet-$Version" ]; then
     wget -qO falconet-$Version.tar.gz https://api.github.com/repos/wondersec/falconet/releases/download/$Version/falconet-$Version.tar.gz || {
-      rm -rf /opt/falconet-$Version.tar.gz
-      echo -e "[\033[31m ERROR \033[0m] Failed to download Falconet (下载 Falconet 失败, 请检查网络是否正常或尝试重新执行脚本)"
-      exit 1
+    rm -rf /opt/falconet-$Version.tar.gz
+    echo -e "[\033[31m ERROR \033[0m] Failed to download Falconet (下载 Falconet 失败, 请检查网络是否正常或尝试重新执行脚本)"
+    exit 1
     }
     tar -xf /opt/falconet-$Version.tar.gz -C /opt || {
-      rm -rf /opt/falconet-$Version
-      echo -e "[\033[31m ERROR \033[0m] Failed to unzip Falconet (解压 Falconet 失败, 请检查网络是否正常或尝试重新执行脚本)"
-      exit 1
+    rm -rf /opt/falconet-$Version
+    echo -e "[\033[31m ERROR \033[0m] Failed to unzip Falconet (解压 Falconet 失败, 请检查网络是否正常或尝试重新执行脚本)"
+    exit 1
     }
     rm -rf /opt/falconet-$Version.tar.gz
   fi
